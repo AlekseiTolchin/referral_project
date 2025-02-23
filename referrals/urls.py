3c3e36b3855d41654a20f61ad1282c5e460a7fea
Aleksey525 <alex_tolchin@mail.ru>
2025-02-24 01:25:59 +0300

    Add removal of referral code by id

--- referrals/urls.py
+++ referrals/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CreateReferralCodeView
+from .views import CreateReferralCodeView, DeleteReferralCodeView
 
 
 urlpatterns = [
     path('referral-codes/', CreateReferralCodeView.as_view(), name='create-code'),
+    path('referral-codes/<int:pk>/', DeleteReferralCodeView.as_view(), name='delete-code')
 ]
--- referrals/views.py
+++ referrals/views.py
@@ -1,8 +1,11 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework import generics, status
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .models import ReferralCode
 from .serializers import ReferralCodeSerializer
 
 
@@ -10,9 +13,44 @@ class CreateReferralCodeView(APIView):
     serializer_class = ReferralCodeSerializer
     permission_classes = [IsAuthenticated]
 
+    @extend_schema(
+        tags=['Реферальные коды'],
+        summary='Создать реферальный код',
+        description='Создание нового реферального кода для текущего пользователя'
+    )
+
     def post(self, request):
         serializer = ReferralCodeSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(user=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DeleteReferralCodeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @extend_schema(
+        tags=['Реферальные коды'],
+        summary='Удалить реферальный код',
+        description='Удаление реферального кода по id'
+    )
+
+    def delete(self, request, pk):
+        try:
+            referral_code = ReferralCode.objects.get(pk=pk)
+        except ReferralCode.DoesNotExist:
+            return Response(
+                {'detail': 'Реферальный код не найден'},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        if referral_code.user != request.user:
+            raise PermissionDenied('Вы не можете удалить этот код')
+
+        referral_code.delete()
+        return Response(
+            {'detail': 'Реферальный код успешно удален'},
+            status=status.HTTP_200_OK
+        )
