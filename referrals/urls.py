14732c6643e83d544b5c14852cd6bfeea93edae4
Aleksey525 <alex_tolchin@mail.ru>
2025-02-25 12:51:47 +0300

    Edit code

--- referrals/urls.py
+++ referrals/urls.py
@@ -5,6 +5,6 @@ from .views import CreateReferralCodeView, DeleteReferralCodeView, GetReferralBy
 urlpatterns = [
     path('referral-codes/', CreateReferralCodeView.as_view(), name='create-code'),
     path('referral-codes/<int:pk>/', DeleteReferralCodeView.as_view(), name='delete-code'),
-    path('referrals-codes/email/', GetReferralByEmailView.as_view(), name='get-code-by-email'),
+    path('referral-codes/email/', GetReferralByEmailView.as_view(), name='get-code-by-email'),
     path('referrals/by-user/<int:user_id>/', UserReferralsView.as_view(), name='referrals-by-user'),
 ]
