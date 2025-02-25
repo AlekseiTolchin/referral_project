ce295349da9d4e8aeb89860ba79b72e794c8c05f
Aleksey525 <alex_tolchin@mail.ru>
2025-02-25 11:26:22 +0300

    Refactor the code

--- referrals/models.py
+++ referrals/models.py
@@ -5,11 +5,15 @@ from django.db import models
 
 
 class ReferralCode(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    code = models.CharField(max_length=20, unique=True, blank=True)
-    expiration_date = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    code = models.CharField(max_length=20, unique=True, blank=True, verbose_name='Реферальный код')
+    expiration_date = models.DateTimeField(verbose_name='Дата окончания действия кода')
     created_at = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Реферальный код'
+        verbose_name_plural = 'Реферальные коды'
 
     def save(self, *args, **kwargs):
         if not self.code:
@@ -23,11 +27,18 @@ class ReferralCode(models.Model):
 
         super().save(*args, **kwargs)
 
+    def __str__(self):
+        return f'Реферальный код пользователя {self.user.username}'
+
 
 class ReferralRelations(models.Model):
-    referrer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='referrals')
-    referral = models.OneToOneField(User, on_delete=models.CASCADE, related_name='referred_by')
+    referrer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='referrals', verbose_name='Реферер')
+    referral = models.OneToOneField(User, on_delete=models.CASCADE, related_name='referred_by', verbose_name='Реферал')
     created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        verbose_name = 'Реферальная связь'
+        verbose_name_plural = 'Реферальные связи'
+
     def __str__(self):
-        return f'{self.referrer} -> {self.referral}'
+        return f'Реферер - {self.referrer} -> Реферал - {self.referral}'
